e0c782869bf77c7e24d0bae411ff262c2518434b
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-02-07 10:38:10 +0530

    fix lints

--- lib/json_extensions.dart
+++ lib/json_extensions.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library json_extensions;
 
 export 'src/json_extensions_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
--- lib/src/json_extensions_base.dart
+++ lib/src/json_extensions_base.dart
@@ -1,22 +1,41 @@
+/// Generic type for json
 typedef Json<T extends Object?> = Map<String, T>;
 
+/// Signature of Callbacks that accept json and return a value;
 typedef FromJsonCallback<T extends Object?> = T Function(Json);
 
+/// Extensions on json type
 extension JsonParse on Json {
+  /// Try to get value at [key] as int. If the key does not exist or value is
+  /// an invalid int, return [fallback].
   int asInt(String key, {int fallback = -1}) =>
       int.tryParse(this[key].toString()) ?? fallback;
 
+  /// Try to get value at [key] as int. If the key does not exist or value
+  /// is an invalid int, return null.
   int? asIntN(String key) => int.tryParse(this[key].toString());
 
+  /// Try to get value at [key] as String. If the key does not exist or value
+  /// is null, return [fallback].
   String asString(String key, {String fallback = ''}) =>
       this[key]?.toString() ?? fallback;
 
+  /// Try to get value at [key] as String. If the key does not exist or value
+  /// is null, return null.
   String? asStringN(String key) => this[key]?.toString();
 
+  /// Try to get value at [key] as bool. If the key does not exist or value is
+  /// null, return [fallback].
   bool asBool(String key, {bool fallback = false}) =>
       this[key] as bool? ?? fallback;
+
+  /// Try to get value at [key] as bool. If the key does not exist or value
+  /// is null, return null.
   bool? asBoolN(String key) => this[key] as bool?;
 
+  /// Try to get value at [key] as DateTime. If the key does not exist or value
+  /// is an invalid DateTime, return [fallback]. If fallback is not passed,
+  /// DateTime.now() is returned as a default value.
   DateTime asDateTime(String key, {DateTime? fallback}) {
     if (this[key] == null) return fallback ?? DateTime.now();
     return DateTime.tryParse(this[key].toString()) ??
@@ -24,59 +43,78 @@ extension JsonParse on Json {
         DateTime.now();
   }
 
+  /// Try to get value at [key] as DateTime. If the key does not exist or value
+  /// is an invalid DateTime, return null.
   DateTime? asDateTimeN(String key) {
     if (this[key] == null) return null;
     return DateTime.tryParse(this[key].toString());
   }
 
+  /// Try to get value at [key] as double. If the key does not exist or value
+  /// is an invalid double, return [fallback].
   double asDouble(String key, {double fallback = -1}) =>
       double.tryParse(this[key].toString()) ?? fallback;
 
+  /// Try to get value at [key] as double. If the key does not exist or value
+  /// is an invalid double, return null.
   double? asDoubleN(String key) => double.tryParse(this[key].toString());
 
+  /// Try to get value at [key] as Map. If the key does not exist or value is
+  /// null, return [fallback].
   Map<K, T> asMap<K, T>(String key, {Map<K, T>? fallback}) =>
       (this[key] as Map? ?? fallback ?? <K, T>{}).cast<K, T>();
 
+  /// Try to get value at [key] as [Map<K, T>]. If the key does not exist or
+  /// value is null, return null.
   Map<K, T>? asMapN<K, T>(String key) => (this[key] as Map?)?.cast<K, T>();
 
+  /// Try to get value at [key] as [Json<T>]. If the key does not exist or value
+  /// is null, return [fallback].
   Json<T> asJson<T extends Object?>(String key, {Json<T>? fallback}) =>
       asMap<String, T>(key, fallback: fallback);
 
+  /// Try to get value at [key] as [Json<T>]. If the key does not exist or value
+  /// is null, return null.
   Json<T>? asJsonN<T extends Object?>(String key) => asMapN<String, T>(key);
 
+  /// Try to get value at [key] as [List<T>]. If the key does not exist or value
+  /// is null, return [fallback]. If fallback is null, then empty [List<T>] is
+  /// returned.
   List<T> asList<T>(String key, {List<T>? fallback}) =>
       (this[key] as List? ?? fallback ?? <T>[]).cast<T>();
 
+  /// Try to get value at [key] as [List<Json>]. If the key does not exist or
+  /// value is null, return [fallback]. If fallback is null, then empty
+  /// [List<Json>] is returned.
   List<Json> asJsonList(String key, {List<Json>? fallback}) =>
       asList<Json>(key, fallback: fallback);
 
+  /// Try to get value at [key] as [List<Json>]. If the key does not exist or
+  /// value is null, return null.
   List<T>? asListN<T>(String key) => (this[key] as List?)?.cast<T>();
 
+  /// Try to get value at [key] as [List<Json>]. If the key does not exist or
+  /// value is null, return null.
   List<Json>? asJsonListN(String key) => asListN<Json>(key);
 
+  /// Check whether the json has a value for [key]
   bool has(String key) => this[key] != null;
 
-  T? asCustom<T extends Object?>(String key, {T? fallback}) =>
-      this[key] as T? ?? fallback;
-
-  T parse<T>(String key, T Function(Json) parser) {
-    try {
-      return parser(asJson(key));
-    } catch (e) {
-      throw Exception('Unable to parse $key as $T');
-    }
+  /// Passes the value at [key] to [parser] and returns the generated value. If
+  /// the value at [key] is null or the parser callback throws an exception, it
+  /// is passed through.
+  T parse<T>(String key, FromJsonCallback<T> parser) {
+    return parser(asJson(key));
   }
 
+  /// Passes the value at [key] to [parser] and returns the generated value. If
+  /// the value at [key] is null or the parser callback throws an exception,
+  /// returns fallback.
   T? parseN<T>(String key, T Function(Json) parser, {T? fallback}) {
     try {
       return parser(asJson(key));
     } catch (e) {
       return fallback;
     }
-  }
-
-  T? makeN<T, P>(P? value, T Function(P) factory, [T? fallback]) {
-    if (value == null) return fallback;
-    return factory(value);
   }
 }
